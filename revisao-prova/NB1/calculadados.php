3cb8f4e0d04b7dce516a5ed6a4543d73a19a2185
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-03-26 20:59:05 -0300

    Tratando possíveis erros

--- revisao-prova/NB1/calculadados.php
+++ revisao-prova/NB1/calculadados.php
@@ -14,34 +14,39 @@
         <center><h2>Resultados</h2></center>
 
         <?php 
+            
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $celular = $_POST['celular'];
+            $salarioBase = $_POST['salario'];
 
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $celular = $_POST['celular'];
-        $salarioBase = $_POST['salario'];
-
-        echo    "Nome: " . $nome . "<br> " .
-                "Email: " . $email .  "<br> " .
-                "Celular: " . $celular . "<br> " .  
-                "Salario: " . $salarioBase . "<br> " .
-                "Reajuste: " . reajuste($salarioBase);
-
-        function reajuste ($salarioBase) {
-            if ($salarioBase < 1501) {
-                return "" . number_format($salarioBase + ($salarioBase * (5.49/100)), 2);
+            if ($nome == "" || $email == "" || $celular == "" || $salarioBase == "") {
+                echo "<h2 style='color:red'>Por favor, preencha todos os componentes!</h2>";
             }
-            if ($salarioBase < 2101) {
-                return "" . number_format($salarioBase + ($salarioBase * (4.93/100)), 2);
-            }
-            if ($salarioBase < 3150) {
-                return "" . number_format($salarioBase + ($salarioBase * (3.75/100)), 2);
-            }
-            if ($salarioBase < 4200) {
-            return "" . number_format($salarioBase + ($salarioBase * (2/100)), 2);
+            else {
+                echo    "Nome: " . $nome . "<br> " .
+                        "Email: " . $email .  "<br> " .
+                        "Celular: " . $celular . "<br> " .  
+                        "Salario: " . $salarioBase . "<br> " .
+                        "Reajuste: " . reajuste($salarioBase);
             }
 
-            return "" . number_format($salarioBase + ($salarioBase * (1.5/100)), 2);
-        }
+            function reajuste ($salarioBase) {
+                if ($salarioBase < 1501) {
+                    return "" . number_format($salarioBase + ($salarioBase * (5.49/100)), 2);
+                }
+                if ($salarioBase < 2101) {
+                    return "" . number_format($salarioBase + ($salarioBase * (4.93/100)), 2);
+                }
+                if ($salarioBase < 3150) {
+                    return "" . number_format($salarioBase + ($salarioBase * (3.75/100)), 2);
+                }
+                if ($salarioBase < 4200) {
+                return "" . number_format($salarioBase + ($salarioBase * (2/100)), 2);
+                }
+
+                return "" . number_format($salarioBase + ($salarioBase * (1.5/100)), 2);
+            }
         ?>
 
     </div>
