9448279982ca556fd82148e1b3e6e5caf91725b2
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-03-26 20:10:08 -0300

    Exercicio completo

---
+++ revisao-prova/NB1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="form">
+        <form action="calculadados.php" method="post">
+
+            Nome: <input type="text" name="nome" id="nome"> <br>
+
+            Email: <input type="text" name="email" id="email"> <br>
+
+            Celular: <input type="text" name="celular" id="celular"> <br>
+
+            Salario: <input type="text" name="salario" id="salario"> <br>
+            
+            <input type="submit">
+            
+        </form>
+    </div>
+    
+</body>
+</html>
