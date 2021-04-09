65aa8081f5349efd38b3f651424b35d74759b4fb
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-04-09 20:01:12 -0300

    Exercicio 2 - NB1

---
+++ NB1/exercicio2.php
@@ -0,0 +1,17 @@
+<?php 
+
+    $valor1 = 10.4;
+    $valor2 = 20.6;
+
+    $adicao = $valor1 + $valor2;
+    $subtracao = $valor1 - $valor2;
+    $multiplicacao = $valor1 * $valor2;
+    $divisao = $valor1 / $valor2;
+
+    echo "Valor 1: " . $valor1 . "<br>Valor 2: " . $valor2 . "<br>";
+
+    echo "<br> Adição: " . $adicao;
+    echo "<br> Subtração: " . $subtracao;
+    echo "<br> Multiplicação: " . $multiplicacao;
+    echo "<br> Divisão: " . $divisao;
+?>
